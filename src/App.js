2bb9f50aea6db5fb0087da2acc6973eba09f9f8c
Harshpal Judge <65704494+harshpaljudge@users.noreply.github.com>
2021-09-06 17:33:20 +0530

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [tronAddress, setTronAddress] = useState("");
+  let [blockChainName, setBlockChainName] = useState("");
+  let [ethereumAddress, setEthereumAddress] = useState("");
+  let [solanaAddress, setSolanaAddress] = useState("");
+
+  function gettronweb() {
+    if (window.tronWeb && window.tronWeb.defaultAddress.base58) {
+      tronAddress = window.tronWeb.defaultAddress.base58;
+      setTronAddress(tronAddress);
+    }
+  }
+
+  function getethereumweb() {
+    if(window.ethereum){
+      window.ethereum.request({ method: 'eth_requestAccounts' });
+      ethereumAddress = window.ethereum.selectedAddress;
+      setEthereumAddress(ethereumAddress);
+      let chainId = window.ethereum.chainId;
+      if(chainId === "0x13881"){
+        blockChainName = "Polygon Matic";
+        setBlockChainName(blockChainName);
+      }
+      else if(chainId === "0x61"){
+        blockChainName = "Binance";
+        setBlockChainName(blockChainName);
+      }
+      else if(chainId === "0x1"){
+        blockChainName = "Ethereum Main Net";
+        setBlockChainName(blockChainName);
+      }
+      else if(chainId === "0x3"){
+        blockChainName = "Ropsten Test Net";
+        setBlockChainName(blockChainName);
+      }
+      else if(chainId === "0x4"){
+        blockChainName = "Rinkeby Test Net";
+        setBlockChainName(blockChainName);
+      }
+    }
+  }
+
+  function getsolanaweb(){
+    window.solana.connect();
+    window.solana.request({ method: "connect" })
+    window.solana.on("connect", () => console.log("connected!"))
+    if(window.solana.publicKey != null)
+    {
+      solanaAddress = window.solana.publicKey.toString();
+      setSolanaAddress(solanaAddress);
+    }
+  }
+    return (
+      <div>
+        <button onClick={gettronweb}>Connect to TronLink</button>
+        <button onClick={getethereumweb}>Connect to MetaMask</button>
+        <button onClick={getsolanaweb}>Connect to Solana</button>
+        
+        <h1> Tron Address: {tronAddress}</h1>
+        <h1> MetaMask Address({blockChainName}): {ethereumAddress}</h1>
+        <h1> Solana Address: {solanaAddress}</h1>
+
+      </div>
+    );
 }
 
 export default App;
